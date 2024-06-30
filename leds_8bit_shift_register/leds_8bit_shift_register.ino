12eecfce0f7d7a40a8d1463b04c6c509222d635b
Bipon Roy <biponroy47@yahoo.com>
2024-06-30 18:14:33 -0400

    leds with 8 bit shift register

---
+++ leds_8bit_shift_register/leds_8bit_shift_register.ino
@@ -0,0 +1,31 @@
+int tDelay = 50;
+int latchPin = 11;  //output register clock
+int clockPin = 9;   // shift register clock
+int dataPin = 12;   //input
+
+byte leds = 0;
+
+void updateShiftRegister() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
+}
+
+void setup() {
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop() {
+  leds = 0;
+  updateShiftRegister();
+  delay(tDelay);
+  for(int i = 0; i < 8; i++){
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(tDelay);
+    bitClear(leds, i);
+    delay(tDelay);
+  }
+}
