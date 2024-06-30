0b7f45962fbe8c30bc520ab512cf310122689118
Bipon Roy <biponroy47@yahoo.com>
2024-06-30 10:43:13 -0400

    updated ultrasonic sensor

--- ultrasonic_sensor/ultrasonic_sensor.ino
+++ ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,32 +1,17 @@
-const int triggerPin = 9;
-const int echoPin = 10;
-long duration;
-int distance;
+#include "SR04.h"
+#define TRIG_PIN 8
+#define ECHO_PIN 10
+SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
+long distance;
 
 void setup() {
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
   Serial.begin(9600);
+  //delay(1000);
 }
 
 void loop() {
-  // Clear the trigger pin
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-
-  // Set the trigger pin high for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(triggerPin, LOW);
-
-  // Read the echo pin and calculate distance
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-
-  // Print the distance to the Serial Monitor
-  Serial.print("Distance: ");
+  distance = sr04.Distance();
   Serial.print(distance);
-  Serial.println(" cm");
-
-  delay(50);
+  Serial.println("cm");
+  delay(1000);
 }
